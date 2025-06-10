b2470eb15b7746fea1830db5a5456f4186e865c4
jooeon-kim <james5459@naver.com>
2025-06-10 17:23:10 +0900

    프톤트 회원가입

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import Admin_PostHiding from "./Admin/Admin_PostHiding";
 import Admin_MatchingList from "./Admin/Admin_MatchingList";
 import Admin_AddCourse from "./Admin/Admin_AddCourse";
 import Course from "./User/Course";
+import Register from "./Common/Register";
 
 
 function App() {
@@ -25,6 +26,8 @@ function App() {
         <Routes>
             <Route path={"/"} element={<Logo/>}></Route>
             <Route path={"/login"} element={<Login/>}></Route>
+            <Route path={"/register"} element={<Register/>}></Route>
+
             <Route path={"/main"} element={<MainLayout/>}>
                 <Route index element={<Intro/>}></Route>
                 <Route path={"users"} element={<Users/>}></Route>
--- frontend/src/mainSlice.js
+++ frontend/src/mainSlice.js
@@ -1,9 +1,10 @@
 import {configureStore, createSlice} from "@reduxjs/toolkit";
 
 const mainSlice = createSlice({
-    name: "slice",
+    name: "main",
     initialState: {
         currentUser: null,
+        token: null,
         matchingInfo: [],
         courses: [],
         users: [
@@ -124,6 +125,15 @@ const mainSlice = createSlice({
 
     },
     reducers: {
+        setToken: (state, action) => {
+            state.token = action.payload;
+        },
+        clearToken: (state) => {
+            state.token = null;
+        },
+        loginUser: (state, action) => {
+            state.currentUser = action.payload;
+        },
         giveLike: (state, action) => {
             const giver = state.currentUser;
             const receiver = state.users.find(e => e.id === action.payload);
@@ -169,9 +179,6 @@ const mainSlice = createSlice({
                     receiver.userMatched.push({id: giver.id});
                 }
             }
-        },
-        loginUser: (state, action) => {
-            state.currentUser = action.payload;
         },
         unlike: (state, action) => {
             const giver = state.currentUser;
@@ -228,7 +235,10 @@ const mainSlice = createSlice({
 
     }
 })
-export const {unlike, giveLike, loginUser, togglePostHidden, matchingInfo, addCourse} = mainSlice.actions;
+export const {
+    giveLike, loginUser, unlike, togglePostHidden,
+    matchingInfo, addCourse, setToken, clearToken
+} = mainSlice.actions;
 const store = configureStore(
     {
         reducer: {
