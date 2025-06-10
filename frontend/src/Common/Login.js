ead91d86933f121d54e010554d66ea71a1f50c8a
jooeon-kim <james5459@naver.com>
2025-06-10 17:49:22 +0900

    로그인

--- frontend/src/Common/Login.js
+++ frontend/src/Common/Login.js
@@ -1,43 +1,63 @@
 import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "../mainSlice";
+import {loginUser, setToken} from "../mainSlice";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
+import apiClient from "../api/apiClient";
+import axios from "axios";
+
 
 function Login() {
     const dispatch = useDispatch();
-    const users = useSelector((state) => state.main.users);
     const navigate = useNavigate();
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
-        const id = e.target.id.value;
-        const pw = e.target.pw.value;
-        const user = users.find((user) => user.id === id && user.pw == pw);
+        const username = e.target.id.value;
+        const password = e.target.pw.value;
 
-        if (user) {
-            alert(`${user.name}님 로그인 성공`);
+        try {
+            const response = await axios.post(
+                "http://localhost:8080/login",
+                { username, password },
+                {
+                    headers: { "Content-Type": "application/json" },
+                    withCredentials: true, // 백엔드가 쿠키 기반 인증이면 필요
+                }
+            );
+
+            // 로그인 성공 시 백엔드에서 토큰과 사용자 정보 받는다고 가정
+            const { token, user } = response.data;
+
+            // 토큰 저장 (localStorage or redux 등)
+            localStorage.setItem("jwtToken", token);
+
+            // 리덕스에 로그인 상태 저장
             dispatch(loginUser(user));
+
+            alert(`${user.usernickname}님 로그인 성공!`);
             navigate("/main");
-        } else {
-            alert("로그인 실패! ID 또는 비밀번호가 틀렸습니다.");
+        } catch (error) {
+            console.error("로그인 에러", error);
+            alert("로그인 실패! 아이디 또는 비밀번호를 확인하세요.");
         }
     };
 
     const goToRegister = () => {
         navigate("/register");
     };
-
     return (
-        <div className="login-container"
-             style={{
-                 backgroundImage: `url(${process.env.PUBLIC_URL}/backgroundintro.jpg)`,
-                 backgroundSize: "cover",
-                 backgroundPosition: "center",
-                 height: "100vh",
-                 display: "flex",
-                 justifyContent: "center",
-                 alignItems: "center",
-             }}>
+        <div
+            className="login-container"
+            style={{
+                backgroundImage: `url(${process.env.PUBLIC_URL}/backgroundintro.jpg)`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
             <div className="overlay">
                 <h1 className="welcome-text">당신의 인연, 오늘 여기에서 시작됩니다.</h1>
                 <form className="login-box" onSubmit={handleLogin}>
@@ -45,7 +65,9 @@ function Login() {
                     <input name="id" type="text" placeholder="아이디" required />
                     <input name="pw" type="password" placeholder="비밀번호" required />
                     <button type="submit">로그인</button>
-                    <button type="button" onClick={goToRegister} className="signup-btn">회원가입</button>
+                    <button type="button" onClick={goToRegister} className="signup-btn">
+                        회원가입
+                    </button>
                 </form>
             </div>
         </div>
--- frontend/src/mainSlice.js
+++ frontend/src/mainSlice.js
@@ -8,122 +8,6 @@ const mainSlice = createSlice({
         matchingInfo: [],
         courses: [],
         hates: [],
-        users: [
-            {
-                id: "aaa",
-                pw: 111,
-                name: "김일번",
-                imgPath: "/man1.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [{id: "ddd"}, {id: "fff"}, {id: "ggg"}],
-                userLiked: [{id: "eee"}],
-                userMatched: []
-            },
-            {
-                id: "bbb",
-                pw: 111,
-                name: "김이번",
-                imgPath: "/man2.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [],
-                userLiked: []
-            },
-            {
-                id: "ccc",
-                pw: 111,
-                name: "김삼번",
-                imgPath: "/man3.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [],
-                userLiked: []
-            },
-            {
-                id: "ddd",
-                pw: 111,
-                name: "김사번",
-                imgPath: "/women1.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [],
-                userLiked: [{id: "aaa"}]
-            },
-            {
-                id: "eee",
-                pw: 111,
-                name: "김오번",
-                imgPath: "/women2.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [{id: "aaa"}],
-                userLiked: []
-            },
-            {
-                id: "fff",
-                pw: 111,
-                name: "김육번",
-                imgPath: "/women3.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [],
-                userLiked: [{id: "aaa"}]
-            },
-            {
-                id: "ggg",
-                pw: 111,
-                name: "김칠번",
-                imgPath: "/man4.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [],
-                userLiked: []
-            },
-            {
-                id: "hhh",
-                pw: 111,
-                name: "김팔번",
-                imgPath: "/women4.png",
-                height: "180",
-                weight: "80",
-                interest: "등산",
-                addr: "서울",
-                intro: "안녕하세요 김일번입니다 잘부탁드립니다",
-                mbti: "estj",
-                userLikedReceived: [],
-                userLiked: [{id: "aaa"}]
-            },
-        ],
-
     },
     reducers: {
         setToken: (state, action) => {
@@ -134,6 +18,10 @@ const mainSlice = createSlice({
         },
         loginUser: (state, action) => {
             state.currentUser = action.payload;
+        },
+        logoutUser: (state) => {
+            state.currentUser = null;
+            state.token = null;
         },
         giveLike: (state, action) => {
             const giver = state.currentUser;
@@ -237,7 +125,7 @@ const mainSlice = createSlice({
     }
 })
 export const {
-    giveLike, loginUser, unlike, togglePostHidden,
+    logoutUser,giveLike, loginUser, unlike, togglePostHidden,
     matchingInfo, addCourse, setToken, clearToken
 } = mainSlice.actions;
 const store = configureStore(
