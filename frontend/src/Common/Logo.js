e779366f58eb1f4492591d2e200cbc427ac9a14a
imseoyeong <fearless40@naver.com>
2025-06-13 16:14:39 +0900

    logo page css update

--- frontend/src/Common/Logo.js
+++ frontend/src/Common/Logo.js
@@ -5,11 +5,11 @@ export default function Logo() {
     const navigate = useNavigate();
 
     return (
-        <div className="logo-container">
+        <section id={"logo-page"}>
             <img src="/logo.png" alt="logo" className="logo-image" />
             <button className="login-button" onClick={() => navigate("/login")}>
                 로그인
             </button>
-        </div>
+        </section>
     );
 }
